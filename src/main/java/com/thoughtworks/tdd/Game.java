6d6eecfce1e78b11baefc6c65c02cac22c604eac
liyijun123 <2510243137@qq.com>
2019-08-23 17:17:27 +0800

    odd

---
+++ src/main/java/com/thoughtworks/tdd/Game.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.tdd;
+
+public class Game {
+	String input= "";
+   public String number(int i) {
+	   
+	   return "1";
+   }
+   public String fizz(int i) {
+	   if(i ==3) {
+		   
+	   }
+	return "fizz";
+   }
+}
---
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.tdd;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+  @Test
+public void should_be_1() {
+	Game game = new Game();
+	
+	String number = game.number(1);
+	String fizz = game.fizz(3);
+	
+	
+	
+}
+}
+
